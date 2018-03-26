9cf629f4c377311df9687642fd913eba0783335f
studnickea19 <36928178+studnickea19@users.noreply.github.com>
2018-03-26 09:03:26 -0500

    Created Computer Class
    Created player sub class for computer

---
+++ RockPaperScissors2/Computer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace RockPaperScissors2
+{
+    public class Computer : Player
+    {
+        public Computer()
+        {
+            //MEMBER VARIABLES (HAS A)
+
+            public string playerName = "Computer";
+            public int playerScore = 0;
+
+            //CONSTRUCTOR (CREATE INSTANCE)
+
+            //MEMBER METHODS (CAN DO)
+
+            //public string TakeTurn()
+            //{
+
+            //};
+        }
+    }
+}
