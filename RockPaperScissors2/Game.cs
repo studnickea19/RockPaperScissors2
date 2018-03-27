8194efd6e5c57147509de07b7df34013f20727ab
studnickea19 <36928178+studnickea19@users.noreply.github.com>
2018-03-27 15:13:33 -0500

    Completed Game
    Game is functioning as planned

--- RockPaperScissors2/Game.cs
+++ RockPaperScissors2/Game.cs
@@ -57,16 +57,15 @@ namespace RockPaperScissors2
 
         public void GetChoices(Player player1, Player player2)
         {
-            for (int turn = 1; turn <= 4; turn++)
+            for (int turn = 0; turn <= 3; turn++)
             {
-                if (turn < 4)
+                if (turn < 3)
                 {
                     GetPlayer1Choice(player1);
                     GetPlayer2Choice(player2);
                     DetermineScore(player1, player2);
-                    turn++;
                 }
-                else if (turn == 4)
+                else if (turn == 3)
                 {
                     FinalScore();
                 }
@@ -76,12 +75,14 @@ namespace RockPaperScissors2
         public Gesture GetPlayer1Choice(Player player1)
         {
             Gesture playerGesture = player1.TakeTurn();
+            Console.WriteLine("Player1 Gesture: {0}", player1.playerGesture);
             return player1.playerGesture;
         }
 
         public Gesture GetPlayer2Choice(Player player2)
         {
             Gesture playerGesture = player2.TakeTurn();
+            Console.WriteLine("Player2 Gesture: {0}", player2.playerGesture);
             return player2.playerGesture;
         }
 
@@ -91,11 +92,13 @@ namespace RockPaperScissors2
             {
                 Console.WriteLine("{0} Wins!", player1.playerName);
                 player1.playerScore++;
+                Console.WriteLine("Player1 Score: {0}",player1.playerScore);
             }
             else if (player1.playerGesture.loseItems.Contains(player2.playerGesture.gestureName))
             {
                 Console.WriteLine("{0} Wins!", player2.playerName);
                 player2.playerScore++;
+                Console.WriteLine("Player2 Score: {0}",player2.playerScore);
             }
             else
             {
